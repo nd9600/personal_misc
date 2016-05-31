c03409e72e6c780f206bc44609e1995fb26fe75b
Nathan Douglas <nd9600@gmail.com>
2016-05-31 20:52:01 +0100

    Spider works, aside from subdomains

--- spider.py
+++ spider.py
@@ -1,55 +1,41 @@
-# import urllib2
-# import time
-# import re
-# from urlparse import urlparse
-# import Queue
-
-# urlQueue = Queue.Queue()
-# urlQueue.put("http://www.reddit.com")
-
-# headers = { 'User-Agent' : 'Mozilla/5.0' }
-# while (not urlQueue.empty()):
-    
-    # request = urllib2.Request(, None, headers)
-    # html = urllib2.urlopen(request).read()
-    # links = re.findall('"((http|https)s?://.*?)"', html)
-    # parsedLinks = [urlparse(link[0]) for link in links]
-
-
-    
-# for link in parsedLinks:
-    # print "Parsed link:", link.netloc
-
-    
 import urllib2
 import time
 import re
 from urlparse import urlparse
+import Queue
 
-originalURL = "http://www.reddit.com"
+hitNetlocs = ["reddit.com"]
+urlQueue = Queue.Queue()
+urlQueue.put("http://www.reddit.com")
+
+count = 0
+
 headers = { 'User-Agent' : 'Mozilla/5.0' }
-request = urllib2.Request(originalURL, None, headers)
-html = urllib2.urlopen(request).read()
-links = re.findall('"((http)s?://.*?)"', html)
-links = [link[0] for link in links]
-
-for link in links:
-    oldNetloc = urlparse(originalURL).netloc
-
-    newScheme = urlparse(link).scheme
-    newNetloc = urlparse(link).netloc
-    newURL = newScheme + "://" + newNetloc
-    print ""
-    print "originalURL:", originalURL
-    print "oldNetloc:", oldNetloc
-    print "Link:", link
-    print "netloc:", newNetloc
-    print "newURL:", newURL
-    print "oldNetloc != newNetloc: ", oldNetloc != newNetloc
+while (not urlQueue.empty()):
     
+    originalURL = urlQueue.get()
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
+    request = urllib2.Request(originalURL, None, headers)
+    try:
+        html = urllib2.urlopen(request).read()
+        links = re.findall('"((http)s?://.*?)"', html)
+        links = [link[0] for link in links]
 
-#parsedLinks = [link[0] for link in links if urlparse(link[0]).netloc != originalURL]
-    
-#for link in parsedLinks:
-    #print "Parsed link:", link
-#time.sleep(2)
+        for link in links:
+            oldNetloc = urlparse(originalURL).netloc
+            newNetloc = urlparse(link).netloc
+                
+            if (newNetloc not in hitNetlocs):
+
+                hitNetlocs.append(newNetloc)
+                count = count + 1
+                
+                newScheme = urlparse(link).scheme
+                newURL = newScheme + "://" + newNetloc
+                urlQueue.put(newURL)
+                print ""
+                print "hitNetlocs:", hitNetlocs
+                print count
+                #time.sleep(0.1)
+    except Exception:
+        pass
