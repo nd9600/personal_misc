9d4fd11d8d9e159df889098f53cbbbb58ffdbde4
Nathan Douglas <nd9600@gmail.com>
2017-11-07 19:55:19 +0000

    routing uses an object!

--- red_framework/framework.r
+++ red_framework/framework.r
@@ -6,20 +6,25 @@ Rebol [
 ;brings in the base FP functions
 do %functional.r
 
-root-dir: what-dir
+;brings in the routing functions into an object called 'routing
+do %routing/routing.r
 
-web-dir: %.   ; the path to where you store your web files
+root_dir: what-dir
+
+web_dir: %.   ; the path to where you store your web files
 controllers-dir: %controllers/
 
 port: 8000
 
-route_methods: ["GET" | "POST" | "HEAD" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH"]
+listen_port: open/lines append tcp://: port  ; port used for web connections
 
-listen-port: open/lines append tcp://: port  ; port used for web connections
+; set up the routes
+routing/get_routes
 
 errors: [
-    400 "Forbidden" "No permission to access:"
-    404 "Not Found" "File was not found:"
+    400 "Forbidden" "No permission to access: "
+    404 "Not Found" "File was not found: "
+    500 "Internal server error" "Error: "
 ]
 
 send-error: function [err-num file] [err] [
@@ -40,15 +45,11 @@ send-page: func [data mime] [
 ; holds the request information which is printed out as connections are made
 buffer: make string! 1024  ; will auto-expand if needed
 
-;brings in the routing functions
-do %routing/routing.r
-routes: get-routes
-
-; processes each HTTP request from a web browser. The first step is to wait for a connection on the listen-port. When a connection is made, the http-port variable is set to the TCP port connection and is then used to get the HTTP request from the browser and send the result back to the browser.
+; processes each HTTP request from a web browser. The first step is to wait for a connection on the listen_port. When a connection is made, the http-port variable is set to the TCP port connection and is then used to get the HTTP request from the browser and send the result back to the browser.
 forever [
     print ""
     print "waiting for request"
-    http-port: first wait listen-port
+    http-port: first wait listen_port
     clear buffer
     print "waiting over"
 
@@ -59,11 +60,13 @@ forever [
     repend buffer ["Address: " http-port/host newline]
     file: "index.html"
     mime: "text/plain"
+    
+    probe routing/accepted_route_methods
 
     ;  parses the HTTP header and copies the requested file name to a variable. This is a very simple method, but it will work fine for simple web server requests.
     parse buffer [
         [
-            copy method route_methods
+            copy method routing/route_methods_rule
         ]
         [
             "http"
@@ -72,49 +75,56 @@ forever [
         ]
     ]
                  
-    route_results: find-route routes method file
+    route_results: routing/find_route method file
     either (not none? route_results) [
         print append copy "route_results are: " mold route_results  
         route: parse route_results/1 "@"
-        route_parameters: route_results/2
-        controller_name: append copy route/1 ".r"
-        controller_function_name: copy route/2
+        
+        either (equal? length? route 1) [
+            print rejoin ["^"" route "^"" " is an incorrect controller"]
+            send-error 500 rejoin ["^"" route "^"" " is an incorrect controller"]
+        ] [
+        
+            route_parameters: route_results/2
+            controller_name: append copy route/1 ".r"
+            controller_function_name: copy route/2
 
-        print append copy "method is: " method 
-        print append copy "file is: " file
-        print append copy "route is: " mold route  
-        print append copy "route_parameters are: " mold route_parameters
+            print append copy "method is: " method 
+            print append copy "file is: " file
+            print append copy "route is: " mold route  
+            print append copy "route_parameters are: " mold route_parameters
 
-        controller_path: append copy controllers-dir controller_name
-        do read controller_path
-        controller_output: do to-word controller_function_name
-        probe controller_output
+            controller_path: append copy controllers-dir controller_name
+            do read controller_path
+            controller_output: do to-word controller_function_name
+            probe controller_output
 
-        ; takes the file's suffix and uses it to lookup the MIME type for the file. This is returned to the web browser to tell it what to do with the data. For example, if the file is foo.html, then a text/html MIME type is returned. You can add other MIME types to this list.
-        parse file [thru "."
-                        [
-                            "html" (mime: "text/html")
-                            | "gif"  (mime: "image/gif")
-                            | "jpg"  (mime: "image/jpeg")
-                        ]
-                   ]
+            ; takes the file's suffix and uses it to lookup the MIME type for the file. This is returned to the web browser to tell it what to do with the data. For example, if the file is foo.html, then a text/html MIME type is returned. You can add other MIME types to this list.
+            parse file [thru "."
+                            [
+                                "html" (mime: "text/html")
+                                | "gif"  (mime: "image/gif")
+                                | "jpg"  (mime: "image/jpeg")
+                            ]
+                       ]
 
-        ; check that the requested file exists, read the file and send it to the browser using the SEND-PAGE function described earlier. If an error occurs, the SEND-ERROR function is called to send the error back to the browser.
-        any [
-            ;if not exists? web-dir/:file [
-            ;    send-error 404 file
-            ;]
-            if error? try [
-                ;data: read/binary web-dir/:file
-                data: copy controller_output
-            ] [
-                send-error 400 file
+            ; check that the requested file exists, read the file and send it to the browser using the SEND-PAGE function described earlier. If an error occurs, the SEND-ERROR function is called to send the error back to the browser.
+            any [
+                ;if not exists? web_dir/:file [
+                ;    send-error 404 file
+                ;]
+                if error? try [
+                    ;data: read/binary web_dir/:file
+                    data: copy controller_output
+                ] [
+                    send-error 400 file
+                ]
+                send-page data mime
             ]
-            send-page data mime
-        ]
 
-        ; makes sure that the connection from the browser is closed, now that the requested web data has been returned.
-        print "port closed"
+            ; makes sure that the connection from the browser is closed, now that the requested web data has been returned.
+            print "port closed"
+        ]
     ] [
         send-error 404 file
     ]
--- red_framework/routing/routing.r
+++ red_framework/routing/routing.r
@@ -2,203 +2,210 @@ Rebol [
     Title: "Tiny Framework: routing"
 ]
 
-accepted_route_methods: ["ANY" "GET" "POST" "HEAD" "PUT" "DELETE" "CONNECT" "OPTIONS" "TRACE" "PATCH"]
+routing: make object! [
 
-get-routes: func [
-    "gets the app's routes"
-    /local current-dir routes
-] [
-    ;changes to the directory where routes are defined first, then changes back after finding the route
-    current-dir: system/options/path
-    change-dir root-dir/routing
-
-    ; 'routes is a series like ["ANY" [] "GET" [] "POST" []]
-    routes: copy/deep accepted_route_methods
-    loop length? routes [routes: insert/only next routes copy []]
-    routes: head routes
-
-    ; loads the data for each routing file
-    route_files: [%routes.r %routes2.r]
-    f_reduce :load route_files
-
-    ; loops through every routing file
-    forall route_files [
-        ; if the current variable is called routes, add its content to the routes hashmap
-        if (equal? first route_files 'routes) [
-            routes_from_this_file: first next route_files
-            foreach actual_route routes_from_this_file [
-
-                ; if the route_method is ANY, GET or POST, add it to the appropriate series
-                ; otherwise, add it to the GET series
-                route_method: first actual_route
-                either (find accepted_route_methods route_method) [
-                    route_url_and_controller: next actual_route
-                    append select routes route_method route_url_and_controller
-                ] [
-                    append select routes "GET" actual_route
-                ]
-            ]
-        ]
-    ]
-
-    ;speeds up finding routes, but the initial creation is slower
-    ;routes: to-map routes
-
-    change-dir current-dir
-    return routes
-]
-
-find-route: func [
-    "gets the route controller for a route URL, checked against the routes for all HTTP methods"
-    routes [series!] "the routes series to search in"
-    route_method [string!] "GET or POST"
-    route_url "the URL of the route"
-    /local routes_for_method route_controller
-] [
-    if (not find accepted_route_methods route_method) [
-        do make error! rejoin [route_method " is not an accepted route method. Only" accepted_route_methods " are accepted"]
-    ]
+    ; used to check if the HTTP request uses an acceptable method
+    accepted_route_methods: copy ["ANY" "GET" "POST" "HEAD" "PUT" "DELETE" "CONNECT" "OPTIONS" "TRACE" "PATCH"]
     
-    ; first checks against "ANY" routes, then the specific route method
-    routes_for_method: select routes "ANY"
-    route_controller_results: get-route-controller routes_for_method route_url
+    ; needed to parse the HTTP request
+    route_methods_rule: copy ["GET" | "POST" | "HEAD" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH"]
     
-    if (not route_controller_results) [
-        routes_for_method: select routes route_method
-        ;print ""
-        ;probe routes
-        ;probe route_method
-        ;probe routes_for_method
-        route_controller_results: get-route-controller routes_for_method route_url
-    ]
-    return route_controller_results
-]
+    routes: copy []
 
-get-route-controller: func [
-    "gets the route controller for a route URL, checked against the routes for a specific HTTP method"
-    routes_for_method [series!] "the routes to check against"
-    url_to_check [string!] "the URL to check"
-    /local route_controller
-] [
-    ; tries to find a route in the ones without parameters first
-    route_controller: select routes_for_method url_to_check
-    
-    ; maybe remove a route if it doesn't have a parameter in it
-    
-    ; if that fails, loop through all other routes - 
-    ;     iterate over the route URL until the tail of it or url_to_check
-    ;         if route_url[i] == url_to_check[i],
-    ;             continue
-    ;         elseif route_url[i] == "{",
-    ;             try to match char after "}" in route_url with first matching char in url_to_check
-    ;             if match,
-    ;                 copy string in between "{" and "}" to variable
-    ;             if no match,
-    ;                 break
-    ;         else,
-    ;             break
-    ;
-    ;         if next chars of route and url_to_check at tail,
-    ;             return head of route
-    ;         else,
-    ;             increment chars
-    ;     reset position of url_to_check
-    ; return none
-    
-    ;print ""
-    ;print url_to_check
-    ;probe routes_for_method
+    get_routes: func [
+        "gets the app's routes"
+        /local current-dir temp_routes
+    ] [
+        ;changes to the directory where routes are defined first, then changes back after finding the route
+        current-dir: system/options/path
+        change-dir root_dir/routing
 
-    if route_controller [
-        return reduce [route_controller []]
-    ]
-    foreach route routes_for_method [
-        parameters: copy []
-            
-        ;print append copy "route: " route
-        while [not any [tail? route tail? url_to_check]] [                 
-            ;probe ""
-            ;probe route
-            ;probe url_to_check
-            any [
-                if (equal? first route first url_to_check) [
-                    ;probe append copy "matched " first route
-                    true
-                ]
+        ; 'routes is a series like ["ANY" [] "GET" [] "POST" []]
+        temp_routes: copy/deep accepted_route_methods
+        loop length? temp_routes [temp_routes: insert/only next temp_routes copy []]
+        routes: head temp_routes
 
-                if (equal? first route #"{") [
-                    ;probe "{ found, matching with }"
-                    
-                    parameter_is_last_thing_in_route: tail? next find route "}"
-                    either parameter_is_last_thing_in_route [
-                        append parameters url_to_check
-                        route: next find route "}"
-                        url_to_check: tail url_to_check
+        ; loads the data for each routing file
+        route_files: [%routes.r %routes2.r]
+        f_reduce :load route_files
+
+        ; loops through every routing file
+        forall route_files [
+            ; if the current variable is called routes, add its content to the routes hashmap
+            if (equal? first route_files 'routes) [
+                routes_from_this_file: first next route_files
+                foreach actual_route routes_from_this_file [
+
+                    ; if the route_method is ANY, GET or POST, add it to the appropriate series
+                    ; otherwise, add it to the GET series
+                    route_method: first actual_route
+                    either (find accepted_route_methods route_method) [
+                        route_url_and_controller: next actual_route
+                        append select routes route_method route_url_and_controller
                     ] [
-                        parameters_match: consume-parameter route url_to_check
-                        ;probe parameters_match
-                        either (parameter_match_in_url != false) [
-                            ;probe "matched with }"
-                            chars_after_end_of_parameter_in_route: parameters_match/1
-                            chars_after_end_of_parameter_in_url: parameters_match/2
-                            parameter_match_in_url: parameters_match/3
-                            
-                            append parameters parameter_match_in_url                                
-                            route: chars_after_end_of_parameter_in_route
-                            url_to_check: chars_after_end_of_parameter_in_url
-                        ] [
-                            break
-                        ]
+                        append select routes "GET" actual_route
                     ]
                 ]
-                break
-            ]
-            
-            either (all [tail? next route tail? next url_to_check]) [
-                route_controller: select routes_for_method head route
-                return reduce [route_controller parameters]
-            ] [
-                route: next route
-                url_to_check: next url_to_check
             ]
         ]
-        url_to_check: head url_to_check
-        ;probe "end"
-    ]
-    return none
-]
 
-consume-parameter: func [
-    "matches and consumes parameters in defined routes and URLs"
-    route [string!] "the defined route with the parameter"
-    url_to_check [string!] "the URL with the parameter"
-    /local parsing_rule
-] [
-    ;if match,
-    ;   return string to between "}"
-    ;if no match,
-    ;   return false
-    
-    ;find the first character after } in the route, and all characters after
-    ;   return false if there is none
-    ;find the characters beginning at that single character in the url_to_check
-    ;   return false if there are none
-    ;parameter match will be all characters up to that point
-    ;return the characters after the parameter in the route and url. and the parameter
-    chars_after_end_of_parameter_in_route: next find route "}"
-    char_after_end_of_parameter_in_route: first chars_after_end_of_parameter_in_route
-    
-    if (chars_after_end_of_parameter_in_route = none) [
-        return false
+        ;speeds up finding routes, but the initial creation is slower
+        ;routes: to-map routes
+
+        change-dir current-dir
     ]
-    
-    chars_after_end_of_parameter_in_url: find url_to_check char_after_end_of_parameter_in_route
-            
-    if (none? chars_after_end_of_parameter_in_url) [
-        return false
+
+    find_route: func [
+        "gets the route controller for a route URL, checked against the routes for all HTTP methods"
+        route_method [string!] "GET or POST"
+        route_url "the URL of the route"
+        /local routes_for_method route_controller
+    ] [
+        if (not find accepted_route_methods route_method) [
+            print rejoin [route_method " is not an accepted route method. Only" accepted_route_methods " are accepted"]
+        ]
+        
+        ; first checks against "ANY" routes, then the specific route method
+        routes_for_method: select routes "ANY"
+        route_controller_results: get_route_controller routes_for_method route_url
+        
+        if (not route_controller_results) [
+            routes_for_method: select routes route_method
+            ;print ""
+            ;probe routes
+            ;probe route_method
+            ;probe routes_for_method
+            route_controller_results: get_route_controller routes_for_method route_url
+        ]
+        return route_controller_results
     ]
-    
-    parse url_to_check [copy parameter_match_in_url to chars_after_end_of_parameter_in_url]
-            
-    return reduce [chars_after_end_of_parameter_in_route chars_after_end_of_parameter_in_url parameter_match_in_url]
+
+    get_route_controller: func [
+        "gets the route controller for a route URL, checked against the routes for a specific HTTP method"
+        routes_for_method [series!] "the routes to check against"
+        url_to_check [string!] "the URL to check"
+        /local route_controller
+    ] [
+        ; tries to find a route in the ones without parameters first
+        route_controller: select routes_for_method url_to_check
+        
+        ; maybe remove a route if it doesn't have a parameter in it
+        
+        ; if that fails, loop through all other routes - 
+        ;     iterate over the route URL until the tail of it or url_to_check
+        ;         if route_url[i] == url_to_check[i],
+        ;             continue
+        ;         elseif route_url[i] == "{",
+        ;             try to match char after "}" in route_url with first matching char in url_to_check
+        ;             if match,
+        ;                 copy string in between "{" and "}" to variable
+        ;             if no match,
+        ;                 break
+        ;         else,
+        ;             break
+        ;
+        ;         if next chars of route and url_to_check at tail,
+        ;             return head of route
+        ;         else,
+        ;             increment chars
+        ;     reset position of url_to_check
+        ; return none
+        
+        ;print ""
+        ;print url_to_check
+        ;probe routes_for_method
+
+        if route_controller [
+            return reduce [route_controller []]
+        ]
+        foreach route routes_for_method [
+            parameters: copy []
+                
+            ;print append copy "route: " route
+            while [not any [tail? route tail? url_to_check]] [                 
+                ;probe ""
+                ;probe route
+                ;probe url_to_check
+                any [
+                    if (equal? first route first url_to_check) [
+                        ;probe append copy "matched " first route
+                        true
+                    ]
+
+                    if (equal? first route #"{") [
+                        ;probe "{ found, matching with }"
+                        
+                        parameter_is_last_thing_in_route: tail? next find route "}"
+                        either parameter_is_last_thing_in_route [
+                            append parameters url_to_check
+                            route: next find route "}"
+                            url_to_check: tail url_to_check
+                        ] [
+                            parameters_match: consume_parameter route url_to_check
+                            ;probe parameters_match
+                            either (parameter_match_in_url != false) [
+                                ;probe "matched with }"
+                                chars_after_end_of_parameter_in_route: parameters_match/1
+                                chars_after_end_of_parameter_in_url: parameters_match/2
+                                parameter_match_in_url: parameters_match/3
+                                
+                                append parameters parameter_match_in_url                                
+                                route: chars_after_end_of_parameter_in_route
+                                url_to_check: chars_after_end_of_parameter_in_url
+                            ] [
+                                break
+                            ]
+                        ]
+                    ]
+                    break
+                ]
+                
+                either (all [tail? next route tail? next url_to_check]) [
+                    route_controller: select routes_for_method head route
+                    return reduce [route_controller parameters]
+                ] [
+                    route: next route
+                    url_to_check: next url_to_check
+                ]
+            ]
+            url_to_check: head url_to_check
+            ;probe "end"
+        ]
+        return none
+    ]
+
+    consume_parameter: func [
+        "matches and consumes parameters in defined routes and URLs"
+        route [string!] "the defined route with the parameter"
+        url_to_check [string!] "the URL with the parameter"
+        /local parsing_rule
+    ] [
+        ;if match,
+        ;   return string to between "}"
+        ;if no match,
+        ;   return false
+        
+        ;find the first character after } in the route, and all characters after
+        ;   return false if there is none
+        ;find the characters beginning at that single character in the url_to_check
+        ;   return false if there are none
+        ;parameter match will be all characters up to that point
+        ;return the characters after the parameter in the route and url. and the parameter
+        chars_after_end_of_parameter_in_route: next find route "}"
+        char_after_end_of_parameter_in_route: first chars_after_end_of_parameter_in_route
+        
+        if (chars_after_end_of_parameter_in_route = none) [
+            return false
+        ]
+        
+        chars_after_end_of_parameter_in_url: find url_to_check char_after_end_of_parameter_in_route
+                
+        if (none? chars_after_end_of_parameter_in_url) [
+            return false
+        ]
+        
+        parse url_to_check [copy parameter_match_in_url to chars_after_end_of_parameter_in_url]
+                
+        return reduce [chars_after_end_of_parameter_in_route chars_after_end_of_parameter_in_url parameter_match_in_url]
+    ]
 ]
