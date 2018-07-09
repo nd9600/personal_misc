f820d1133a12e4c536c19deb98bd881ccc2151d3
Nathan Douglas <9141675+nd9600@users.noreply.github.com>
2018-07-09 13:40:07 +0100

    added more useful functions

--- misc/useful_functions.sh
+++ misc/useful_functions.sh
@@ -14,7 +14,33 @@ gpush() {
     git push
 }
 
-gcheckout() {
+gbranch() {
 	git checkout -b "$1"
-	git push -u
+        git push -u # sets the local's branches upstream
+}
+
+gshow() {
+    re='^[0-9]+$'
+    if [[ -z "$1" ]] || (! [[ $1 =~ $re ]])
+    then
+        git show
+    else
+        git show HEAD~"$1"
+    fi
+}
+
+container_freetobook() {
+    docker exec -it freetobook-docker_php_1 bash
+}
+
+container_freetobook_logs() {
+    docker exec -it freetobook-docker_php_1 tail -f /var/log/php_error_log
+}
+
+container_portal() {
+    docker exec -it freetobook-docker_php_portal_1 bash
+}
+
+container_redis_flushall() {
+    docker exec -it freetobook-docker_redis_portal_1 redis-cli flushall
 }
