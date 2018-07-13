8cc51ac5712647d509865c46fdc444e5ef2713c5
Nathan Douglas <nd9600@gmail.com>
2018-07-13 12:08:11 +0100

    added fzf commands

--- misc/useful_functions.sh
+++ misc/useful_functions.sh
@@ -44,3 +44,27 @@ container_portal() {
 container_redis_flushall() {
     docker exec -it freetobook-docker_redis_portal_1 redis-cli flushall
 }
+
+##fzf
+
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fe() {
+  local files
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+fzfc() {
+  #find ~/ | fzf-tmux | xargs "$1" # searches home dir
+  fzf-tmux | xargs "$1"
+}
